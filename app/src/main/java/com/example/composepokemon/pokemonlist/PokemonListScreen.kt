2a26d801cc63dfd24c493e2a21a2feb49e768566
SeungHyun <s20035@gsm.hs.kr>
2022-06-06 16:56:41 +0900

    coil 오류

--- app/src/main/java/com/example/composepokemon/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/composepokemon/pokemonlist/PokemonListScreen.kt
@@ -126,6 +126,7 @@ fun PokemonList(
     val endReached by remember{viewModel.endReached}
     val loadError by remember{viewModel.loadError}
     val isLoading by remember{viewModel.endReached}
+    val isSearching by remember{viewModel.isSearching}
 
     LazyColumn(contentPadding = PaddingValues(16.dp)){
         val itemCount = if(pokemonList.size % 2 == 0) {
@@ -134,7 +135,7 @@ fun PokemonList(
             pokemonList.size /2+1
         }
         items(itemCount){
-            if(it >= itemCount - 1 && !endReached && !isLoading){
+            if(it >= itemCount - 1 && !endReached && !isLoading && !isSearching){
                 viewModel.loadPokemonPaginated()
             }
             PokedexRow(rowIndex = it, entries = pokemonList, navController =navController)
--- app/src/main/java/com/example/composepokemon/pokemonlist/PokemonListViewModel.kt
+++ app/src/main/java/com/example/composepokemon/pokemonlist/PokemonListViewModel.kt
@@ -14,6 +14,7 @@ import com.example.composepokemon.repository.PokemonRepository
 import com.example.composepokemon.util.Constants.PAGE_SIZE
 import com.example.composepokemon.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
@@ -30,8 +31,38 @@ class PokemonListViewModel @Inject constructor(
     var isLoading = mutableStateOf(false)
     var endReached = mutableStateOf(false)
 
+    private var cachedPokemonList = listOf<PokedexListEntry>()
+    private var isSearchStarting = true
+    var isSearching = mutableStateOf(false)
+
     init {
         loadPokemonPaginated()
+    }
+
+    fun searchPokemonList(query:String){
+        val listToSearch = if(isSearchStarting){
+            pokemonList.value
+        }else{
+            cachedPokemonList
+        }
+        viewModelScope.launch(Dispatchers.Default) {
+            if(query.isEmpty()){
+                pokemonList.value = cachedPokemonList
+                isSearching.value = false
+                isSearchStarting = true
+                return@launch
+            }
+            val results = listToSearch.filter {
+                it.pokemonName.contains(query.trim(), ignoreCase = true) ||
+                        it.number.toString() == query.trim()
+            }
+            if (isSearchStarting){
+                cachedPokemonList = pokemonList.value
+                isSearchStarting = false
+            }
+            pokemonList.value = results
+            isSearching.value = true
+        }
     }
 
     fun loadPokemonPaginated(){
